92688ffb36b7a899d0af403a6ee344b1b99c2604
Aylen <lag9512mail.com>
2023-06-29 14:03:02 -0300

    se arregló el problema de la imagen

--- secretosCulinarios/app.js
+++ secretosCulinarios/app.js
@@ -31,7 +31,7 @@ app.use(session({
   secret: 'asdjgesougbjnsdf123',
   resave: false,
   saveUninitialized: false,
-  cookie: {maxAge: 60000}
+  cookie: {maxAge: 100000}
 }))
 
 // view engine setup
